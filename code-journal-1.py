a437b0c60bea831af8a9e4d8399d93a746358a43
PrIceZeng-816 <lzeng13@ucsc.edu>
2025-11-21 14:02:07 -0800

    Update code-journal-1.py

--- code-journal-1.py
+++ code-journal-1.py
@@ -9,3 +9,7 @@ print("name:", name)
 print("pronouns:", pronouns)
 print(f"One of my absolute favorite movies is '{favorite_movie}'. I just watched it recently and it's always amazing.")
 print(f"one of my favorite foods is {favorite_food}. Though very basic, it's an absolute classic.")
+
+
+
+print("its me Lingchen")
